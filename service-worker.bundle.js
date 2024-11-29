67b84f47d2b1abbf2c4e6831288aec7818b4ba2e
mattfors <mattfors@users.noreply.github.com>
2024-11-29 22:55:38 +0000

    deploy: ebd03771eebe1fb6ab5f2249757c3e517f2af403

--- service-worker.bundle.js
+++
@@ -1,1 +0,0 @@
-(()=>{const e=["/","/index.html","/styles.css","/bundle.js","/icon-192x192.png","/icon-512x512.png"];self.addEventListener("install",(n=>{n.waitUntil(caches.open("vanillajs-experiment-cache-v1").then((n=>n.addAll(e))))})),self.addEventListener("fetch",(e=>{e.respondWith(caches.match(e.request).then((n=>n||fetch(e.request))))}))})();
